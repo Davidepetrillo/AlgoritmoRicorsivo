7a61d4dce20c022e936231ef8e83adb80a415b35
davidepetrillo <davide96petrillo@gmail.com>
2022-10-03 14:37:58 +0200

    Add project files.

---
+++ AlgoritmoRicorsivo/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Algoritmo Ricorsivo - La stessa funzione del programma chiama la propria per progredire fino a quando 
+ * non raggiunge una certa condizione e passo dopo passo ritorna alla fine
+ */
+
+using System.Diagnostics;
+class TestFactorial
+{
+
+    public static void Main(string[] args)
+    {
+        int n = 5;
+        long fac = factorial(n);
+        Console.WriteLine("The factorial of 5 is: " + fac);
+    }
+
+    public static long factorial(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return factorial(n - 1) * n;        }
+    }
+}
